13fd5966db3c17e96d3551e0b839fe3b0d5df10e
Jacob Purcell <jacobdpurcell@outlook.com>
2026-05-28 11:28:09 -0400

    ode wrapper, begin GLRK adaptive step

--- test/diffeq_solvers.jl
+++ test/diffeq_solvers.jl
@@ -103,63 +103,57 @@ Runge-Kutta, dynamical evolution of systems that can be cast as an array of ODEs
 
 """
 
-function glrka(f,y0::AbstractVector,ti::Number=0.,tf::Number=10.,n::Int=1000,s::Int=10,fargs::AbstractVector=[],xn::Int=100000)
+function glrka(f,y0::AbstractVector,ti::Number=0.,tf::Number=10.,s::Int=10,fargs::AbstractVector=[],xn::Int=100000)
     y = y0
     yt = y
     
     yl = Array{typeof(y0[1]),2}(undef,n,length(y0))
     yl[1,:] = y0
 
-    t = collect(range(ti,tf,n))
-    dt = t[2]-t[1]
+    t = ti
+    dt = 1/100
 
     # calculating the butcher-tableau may need to be moved to its own function
     # set up polynomial in ascending order, index is polynomial power
     p1 = normalized_legendre_poly_coef(s) 
-    p2 = normalized_legendre_poly_coef(s+1) 
 
     
     # find roots of polynomial
-    roots1 = polyroots(p)
-    roots2 = polyroots(p)
+    roots = polyroots(p)
 
-    aij1 = zeros(length(roots),length(roots))
-    bi1 = zeros(length(roots))
-    lj1 = zeros(xn)
-    li1 = zeros(xn)
+    aij = zeros(length(roots),length(roots))
+    bi = zeros(length(roots))
+    lj = zeros(xn)
+    li = zeros(xn)
 
-    aij2 = zeros(length(roots),length(roots))
-    bi2 = zeros(length(roots))
-    lj2 = zeros(xn)
-    li2 = zeros(xn)
-
-    for i in 1:length(roots1)
-        for j in 1:length(roots1)
-            lj1 = [lagrange_poly(roots1,x,j) for x in range(0,roots1[i],xn)]
+    for i in 1:length(roots)
+        for j in 1:length(roots)
+            lj = [lagrange_poly(roots,x,j) for x in range(0,roots[i],xn)]
             
             # try higher accuracy integrators to reduce iteration count
-            aij1[i,j] = riemann_integration(range(0,roots1[i],xn),lj1)
+            aij[i,j] = riemann_integration(range(0,roots[i],xn),lj)
         end
-        li1 = [lagrange_poly(roots1,x,i) for x in range(0,1,xn)]
-        bi1[i] = riemann_integration(range(0,1,xn),li1)
-    end
-
-    for i in 1:length(roots2)
-        for j in 1:length(roots2)
-            lj1 = [lagrange_poly(roots2,x,j) for x in range(0,roots2[i],xn)]
-            
-            # try higher accuracy integrators to reduce iteration count
-            aij2[i,j] = riemann_integration(range(0,roots2[i],xn),lj2)
-        end
-        li2 = [lagrange_poly(roots2,x,i) for x in range(0,1,xn)]
-        bi2[i] = riemann_integration(range(0,1,xn),li2)
+        li = [lagrange_poly(roots,x,i) for x in range(0,1,xn)]
+        bi[i] = riemann_integration(range(0,1,xn),li)
     end
 
     # compute runge kutta
     # TODO: add adaptive check
-    for nx in 2:n
-        yi = [y + sum([aij[i,j]*f(y,t[nx],fargs) for j in 1:length(roots)])*dt for i in 1:length(roots)]
-        y = y + sum([bi[i]*f(y,t[nx],fargs) for i in 1:length(roots)])*dt
+    while t < tf
+        yi = [y + sum([aij[i,j]*f(y,t+dt,fargs) for j in 1:length(roots)])*dt for i in 1:length(roots)]
+        y = y + sum([bi[i]*f(y,t+dt,fargs) for i in 1:length(roots)])*dt
+
+        yi = [y + sum([aij[i,j]*f(y,t+dt/2,fargs) for j in 1:length(roots)])*dt for i in 1:length(roots)]
+        y = y + sum([bi[i]*f(y1,t+dt/2,fargs) for i in 1:length(roots)])*dt
+
+        err = abs.((y1.-y2)./2)
+        if maximum(err) < vtol
+            yl = vcat(yl,yt)
+            y = y4
+            t = t + dt
+            tl = vcat(tl,t)
+        end
+        dt = s*dt*((vtol/maximum(err))^(1/5))
 
         yl[nx,:] = y
     end
@@ -394,7 +388,6 @@ end
 - `y0::AbstractVector`: Initial conditions.
 - `ti::Number`: Initial time.
 - `tf::Number`: End time. 
-- `n::Int`: The number of timesteps.
 - `fargs::AbstractVector`: Additional parameters to pass to f.
 - `vtol::AbstractFloat`
 - `s::AbstractFloat` Safety factor, 
@@ -408,11 +401,11 @@ where y1t = [yt1 yt2 ... ytn].
 
 # Description
 Dormand-Price Runge-Kutta ODE solver[^list-of-runge-kutt-methods], dynamical evolution of systems that can be cast 
-as an array of ODEs. This method has an adaptive time step.
+as an array of ODEs. This method has an adaptive time step[^adaptive-runge-kutta-methods].
 
 # References
 [^list-of-runge-kutt-methods]: [List of Runge-Kutta Methods, https://en.wikipedia.org/wiki/List_of_Runge%E2%80%93Kutta_methods (accessed April 14, 2026).](https://en.wikipedia.org/wiki/List_of_Runge%E2%80%93Kutta_methods)
-https://www.youtube.com/watch?v=6bCBXvsD7gw
+[^adaptive-runge-kutta-methods]: [Adaptive Runge-Kutta Methods, https://www.youtube.com/watch?v=6bCBXvsD7gw](https://www.youtube.com/watch?v=6bCBXvsD7gw)
 """
 function rkdp(f,y0::AbstractVector,ti::Number=0.,tf::Number=10.,fargs::AbstractVector=[],vtol::AbstractFloat=1E-10,s::AbstractFloat=0.9) 
     t = ti
@@ -473,23 +466,36 @@ function rkdp(f,y0::AbstractVector,ti::Number=0.,tf::Number=10.,fargs::AbstractV
     return yl,tl
 end
 
-function ode(mode="rkdp")
-    if mode == "rkdp"
-        return rkdp()
-    elseif mode == "rk4"
-        return rk4()
-    elseif mode == "glrk"
-        return glrk()
-    elseif mode == "glrka"
-        return glrka()
-    elseif mode == "rk1"
-        return rk1()
-    elseif mode == "rk23"
-        return rk23()
-    elseif mode == "rk2"
-        return rk2()
-    elseif mode == "rk3"
-        return rk3()
-    end
+"""
+    ode(args...;f=rkdp,kwargs...)
+
+# Arguments
+- `args...`: Arguments for the RK method. 
+- `f`: The name of the RK method to be used
+    - rk1: 1th order.
+    - rk2: 2th order.
+    - rk3: 3th order.
+    - rk4: 4th order.
+    - rkdp: Dormand-Prince (Default).
+    - glrk: Gauss-Legendre.
+    - glrka: Adaptive Gauss-Legendre.
+- `kwargs...`: Positional arguments for the RK method.
+
+
+# Return 
+The time evolution of supplied parameters and the corresponding time series. 
+Each index of the position series is a time evolution of one of the input parameters.
+For example, if an initial condition is specified [y1 y2], then the output is [y1t y2t] 
+where y1t = [yt1 yt2 ... ytn].
+
+# Description
+Wrapper for the Runge-Kutta differential equation solver methods.
+
+# References
+[^list-of-runge-kutt-methods]: [List of Runge-Kutta Methods, https://en.wikipedia.org/wiki/List_of_Runge%E2%80%93Kutta_methods (accessed April 14, 2026).](https://en.wikipedia.org/wiki/List_of_Runge%E2%80%93Kutta_methods)
+[^adaptive-runge-kutta-methods]: [Adaptive Runge-Kutta Methods, https://www.youtube.com/watch?v=6bCBXvsD7gw](https://www.youtube.com/watch?v=6bCBXvsD7gw)
+"""
+function ode(args...;f=rkdp,kwargs...)
+    return f(args...;kwargs...)
 end
 
